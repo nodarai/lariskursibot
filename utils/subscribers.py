f21ca8accf2d745b83afa7bd8bafa985a4765d93
Nodar Kasradze <nod32arai@gmail.com>
2020-03-13 00:16:56 +0400

    Add new db models

--- utils/subscribers.py
+++ utils/subscribers.py
@@ -1,6 +1,13 @@
-#! /usr/bin/env python
 # coding: utf-8
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Float,
+    Date,
+    ForeignKey,
+    create_engine,
+)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
@@ -11,7 +18,26 @@ class Subscriber(Base):
     # Columns
     id = Column(Integer, primary_key=True)
     chat_id = Column(String(20), nullable=False, unique=True)
-    
+
+
+class RefCurrency(Base):
+    __tablename__ = "refcurrency"
+
+    id = Column(Integer, primary_key=True)
+    code = Column(String(3))
+    name = Column(String(100))
+    quantity = Column(Integer)
+
+
+class Rate(Base):
+    __tablename__ = "rate"
+
+    id = Column(Integer, primary_key=True)
+    rate = Column(Float)
+    currency_id = Column(Integer, ForeignKey('refcurrency.id'))
+    date = Column(Date)
+
+
 engine = create_engine('sqlite:///subscribers.db')
 
 Base.metadata.create_all(engine)
