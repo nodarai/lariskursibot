7776abfd34bc32be72c94ae31a7c48be2a30e748
Nodar Kasradze <nod32arai@gmail.com>
2021-01-18 02:18:28 +0400

    Add Celery Beat for periodic tasks

---
+++ celery_tasks.py
@@ -0,0 +1,30 @@
+from celery import Celery
+from celery.schedules import crontab
+from telegram.ext import Updater
+
+from kursi import inform_subscribers, TOKEN
+from utils.models import DB_FILENAME, initialize_db
+
+
+app = Celery(
+    'lariskursibot',
+    broker=f'sqla+{DB_FILENAME}',
+    timezone='Asia/Tbilisi'   
+)
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Executes everyday at 17:05 Tbilisi time (UTC+4)
+    sender.add_periodic_task(
+        crontab(hour=17, minute=5),
+        daily_informer.s(),
+    )
+
+
+@app.task
+def daily_informer():
+    db_session = initialize_db()
+    updater = Updater(token=TOKEN)
+    inform_subscribers(updater.bot, db_session)
+    db_session.close()
